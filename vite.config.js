1f362c06616870d2774c1376199073bf73ae4d7b
jim925d <james.mcguirk@ucdenver.edu>
2026-02-23 17:58:55 -0700

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.',
+  base: '/GTM-Dashboard/', // for GitHub Pages: https://jim925d.github.io/GTM-Dashboard/
 });
